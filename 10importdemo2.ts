831a2000231da30f028ee42b6eea29ccaa24162a
AnishaWatts <anishawatts77@gmail.com>
2025-10-15 12:08:56 +0530

    Second Commit

---
+++ 10importdemo2.ts
@@ -0,0 +1,5 @@
+import { SavingsAccount } from "./8typescriptclass.js";
+
+let Savings = new SavingsAccount();
+Savings.AccDetails("Anisha", 2333, 0.05);
+Savings.showDetails();
---
+++ Arrowfunctiondemo.js
@@ -0,0 +1,26 @@
+function getNAme() {
+    console.log("HEHE");
+}
+
+function search(id) {
+    if (id == 100) {
+        console.log("Available");
+    } else {
+        console.log("Not Found");
+    }
+}
+
+function getROI() {
+    return 0.05;
+}
+let clientName = () => console.log("hello");
+let searchbyid = (id) => {
+    if (id == 100) console.log("Present");
+    else console.log("Not present");
+}
+let getR = () => 0.05;
+getNAme();
+search(100);
+console.log(getROI());
+
+
